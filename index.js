bc2983c0d88800bd392d1c27ecfe2f0e44224180
Nandini Srivastava <44871328+Nandinisri@users.noreply.github.com>
2020-05-08 20:50:03 +0530

    My new game

---
+++ index.js
@@ -0,0 +1,91 @@
+
+  function func()
+  {
+    var n = document.getElementById("myText").value;
+    document.getElementById("demo").innerHTML = "The no.of balls left in the bag are : " + n ;
+    document.getElementById("start").innerHTML = "All the best ..<br>";
+    document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp 😉 Player 1 is Playing ..<br>";
+  }
+    var res = 0;
+    var count = 0;
+    var button1 = document.getElementById("countButton1");
+    var button2 = document.getElementById("countButton2");
+    var button3 = document.getElementById("countButton3");
+    button1.onclick = function(){
+      count++;
+      if(count % 2==0){
+        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp  NOW 👻 PLAYER 2 IS PLAYING  ..<br>";
+      }
+      else {
+        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp 😉 PLAYER 1 IS PLAYING ..<br>";
+      }
+      var n1 = document.getElementById("countButton1").value;
+      var n = document.getElementById("myText").value;
+      res= res +  Number(n1);
+      val = Number(n) - res;
+      document.getElementById("demo").innerHTML = "The no.of balls left in the bag are : " + val  ;
+
+      if(val == 0){
+        if(count%2!=0){
+          document.getElementById("remaining").innerHTML = "";
+          document.getElementById("player1").innerHTML = "👏🎈🎉🎊😉 PLAYER 1 Is WINNER !!..👏🎈🎉🎊😉";
+        }
+        else{
+          document.getElementById("remaining").innerHTML = "";
+          document.getElementById("player1").innerHTML = "👏🎈🎉🌹👻 PLAYER 2 Is WINNER !!..👏🎈🎉🌹👻";
+        }
+      }
+    }
+
+    button2.onclick = function(){
+      count++;
+      if(count % 2==0)
+      {
+        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp  NOW 👻 PLAYER 2 IS PLAYING   ..<br>";
+      }
+      else {
+        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp 😉 PLAYER 1 IS PLAYING ..<br>";
+      }
+
+      var n2 = document.getElementById("countButton2").value;
+      var n = document.getElementById("myText").value;
+      res= res +  Number(n2);
+      val = Number(n) - res;
+      document.getElementById("demo").innerHTML = "The no.of balls left in the bag are : " + val  ;
+      if(val == 0){
+        if(count%2!=0){
+          document.getElementById("remaining").innerHTML = "";
+          document.getElementById("player1").innerHTML = "👏🎈🎉🎊😉 PLAYER 1 Is WINNER !!..👏🎈🎉🎊😉";
+        }
+        else{
+          document.getElementById("remaining").innerHTML = "";
+          document.getElementById("player1").innerHTML = "👏🎈🎉🌹👻 PLAYER 2 Is WINNER !!..👏🎈🎉🌹👻";
+        }
+      }
+    }
+
+    button3.onclick = function(){
+      count++;
+      if(count % 2==0)
+      {
+        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp  NOW 👻 PLAYER 2 IS PLAYING   ..<br>";
+      }
+      else {
+        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp 😉 PLAYER 1 IS PLAYING..<br>";
+      }
+      var n3 = document.getElementById("countButton3").value;
+      var n = document.getElementById("myText").value;
+      res= res +  Number(n3);
+      val = Number(n) - res;
+      document.getElementById("demo").innerHTML = "The no.of balls left in the bag are : " + val  ;
+      if(val == 0){
+        if(count%2!=0){
+          document.getElementById("remaining").innerHTML = "";
+          document.getElementById("player1").innerHTML = "👏🎈🎉🎊😉 PLAYER 1 Is WINNER !!..👏🎈🎉🎊😉";
+        }
+        else{
+          document.getElementById("remaining").innerHTML = "";
+          document.getElementById("player1").innerHTML = "👏🎈🎉🌹👻 PLAYER 2 Is WINNER !!..👏🎈🎉🌹👻";
+        }
+      }
+    }
