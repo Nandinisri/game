99051c391a5f2e4b5dad8fab825e2912f4c33201
Nandini Srivastava <44871328+Nandinisri@users.noreply.github.com>
2020-05-09 11:24:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
     var n = document.getElementById("myText").value;
     document.getElementById("demo").innerHTML = "The no.of balls left in the bag are : " + n ;
     document.getElementById("start").innerHTML = "All the best ..<br>";
-    document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp 😉 Player 1 is Playing ..<br>";
+    document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp 😉 Player 1 will start ..<br>";
   }
     var res = 0;
     var count = 0;
@@ -13,11 +13,11 @@
     var button3 = document.getElementById("countButton3");
     button1.onclick = function(){
       count++;
-      if(count % 2==0){
-        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp  NOW 👻 PLAYER 2 IS PLAYED  ..<br>";
+      if(count % 2!==0){
+        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp  NOW IT'S 👻 PLAYER 2 TURN  ..<br>";
       }
       else {
-        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp 😉 PLAYER 1 IS PLAYED ..<br>";
+        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp IT'S😉 PLAYER 1 TURN ..<br>";
       }
       var n1 = document.getElementById("countButton1").value;
       var n = document.getElementById("myText").value;
@@ -39,12 +39,12 @@
 
     button2.onclick = function(){
       count++;
-      if(count % 2==0)
+      if(count % 2!==0)
       {
-        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp  NOW 👻 PLAYER 2 IS PLAYED   ..<br>";
+        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp  NOW 👻 PLAYER 2 IS PLAYING   ..<br>";
       }
       else {
-        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp 😉 PLAYER 1 IS PLAYED ..<br>";
+        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp 😉 PLAYER 1 IS PLAYING ..<br>";
       }
 
       var n2 = document.getElementById("countButton2").value;
@@ -68,10 +68,10 @@
       count++;
       if(count % 2==0)
       {
-        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp  NOW 👻 PLAYER 2 IS PLAYED  ..<br>";
+        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp  NOW  IT'S 👻 PLAYER 2 TURN  ..<br>";
       }
       else {
-        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp 😉 PLAYER 1 IS PLAYED..<br>";
+        document.getElementById("player1").innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp IT'S 😉 PLAYER 1 TURN..<br>";
       }
       var n3 = document.getElementById("countButton3").value;
       var n = document.getElementById("myText").value;
